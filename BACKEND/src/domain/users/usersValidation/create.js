6660a06c4ad87dfcab668f8588c0a277fb2e9ed1
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-14 20:05:06 -0300

    correção de validação

--- BACKEND/src/domain/users/usersValidation/create.js
+++ BACKEND/src/domain/users/usersValidation/create.js
@@ -3,8 +3,8 @@ import { validate, Joi } from "express-validation";
 export default validate({
    body: Joi.object({
       name: Joi.string().required(),
-      email: Joi.string().required(),
-      apartament: Joi.string().required(),
+      email: Joi.string().email().required(),
+      apartament: Joi.number().required(),
       password: Joi.string().required(),
    }),
 });
