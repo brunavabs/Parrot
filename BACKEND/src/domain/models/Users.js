0b05a3cca62d70fd21652c20d0fc9a9ac33849bb
Isabela <isabela.ribeiro@outlook.com>
2022-06-14 20:14:21 -0300

    Validacao do email

--- BACKEND/src/domain/models/Users.js
+++ BACKEND/src/domain/models/Users.js
@@ -14,6 +14,7 @@ const Users = db.define(
       },
       email: {
          type: DataTypes.STRING,
+         unique: true,
       },
       apartament: {
          type: DataTypes.INTEGER,
--- BACKEND/src/domain/users/userCase/create/CreateUserUseCase.js
+++ BACKEND/src/domain/users/userCase/create/CreateUserUseCase.js
@@ -4,6 +4,12 @@ import { hashPassword } from "../../../../shared/utils/password/password.js";
 export class CreateUserUseCase {
   static async registroUser(data) {
     const { name, email, apartament, password } = data;
+    const count = await User.count({ where: { email } });
+
+    if (count) {
+      return false;
+    }
+
     const newPassword = hashPassword(password);
 
     const novoUser = await User.create({
