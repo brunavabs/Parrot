fff3138d0fa7f90b85f795308834adaba5d85dc8
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-13 22:32:07 -0300

    adição das rotas app

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import db from './infra/database/index.js';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import db from "./infra/database/index.js";
+import routes from "./routes/index.js";
 
 db.hasConnection();
 
-const app = express()
+const app = express();
 
 app.use(cors());
+app.use(express.json())
 
-
-
+app.use(routes);
 
 export default app;
