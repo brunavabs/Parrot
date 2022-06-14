22bc7695aa03b25dbd47de1aa80758682ce3f7ae
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-14 15:57:51 -0300

    removendo responsibilidade de hashpw do useCase

--- BACKEND/src/domain/users/userCase/create/CreateUserUseCase.js
+++ BACKEND/src/domain/users/userCase/create/CreateUserUseCase.js
@@ -1,10 +1,10 @@
 import User from "../../../models/Users.js";
-import bcrypt from "bcrypt";
+import { hashPassword } from "../../../../shared/utils/password/password.js";
 
 export class CreateUserUseCase {
   static async registroUser(data) {
     const { nome, email, apartament, password } = data;
-    const newPassword = bcrypt.hashSync(password, 10);
+    const newPassword = hashPassword(password);
 
     const novoUser = await User.create({
       nome,
