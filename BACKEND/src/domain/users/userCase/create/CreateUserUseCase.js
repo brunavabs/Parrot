c0ea6c889d57271edf3e3078383efa0e80cd12fe
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-14 16:51:35 -0300

    correção de nome

--- BACKEND/src/domain/users/userCase/create/CreateUserUseCase.js
+++ BACKEND/src/domain/users/userCase/create/CreateUserUseCase.js
@@ -3,11 +3,11 @@ import { hashPassword } from "../../../../shared/utils/password/password.js";
 
 export class CreateUserUseCase {
   static async registroUser(data) {
-    const { nome, email, apartament, password } = data;
+    const { name, email, apartament, password } = data;
     const newPassword = hashPassword(password);
 
     const novoUser = await User.create({
-      nome,
+      name,
       email,
       apartament,
       password: newPassword,
