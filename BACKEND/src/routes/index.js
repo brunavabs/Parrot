7532964e680ec5339387dd47cc2dcfa1f795a084
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-15 00:32:47 -0300

    criação da rota de login

--- BACKEND/src/routes/index.js
+++ BACKEND/src/routes/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import userRoutes from "./usersRoutes.js";
 import postsRoutes from "./postsRoutes.js";
+import { AuthController } from "../domain/auth/authUseCase/authContoller.js";
+import loginValidation from "../domain/auth/authValidation/index.js";
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-   res.status(200).json({msg:'teste rotas'})
-})
+routes.post("/login", loginValidation.login, AuthController.login);
 
 routes.use(userRoutes);
 routes.use(postsRoutes);
