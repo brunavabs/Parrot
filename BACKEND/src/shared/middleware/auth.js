81a265cc4e56060fcf59261a42616a184e26d6cb
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-15 17:32:56 -0300

    adição da extensão .js

--- BACKEND/src/shared/middleware/auth.js
+++ BACKEND/src/shared/middleware/auth.js
@@ -1,5 +1,5 @@
 import { expressjwt } from "express-jwt";
-import secret from "../../config/secret";
+import secret from "../../config/secret.js";
 
 export default expressjwt({
    secret: secret.key,
