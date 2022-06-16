e5e52e64073f4ec60b0ec2cf978653dc8db3dcb1
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-16 16:43:46 -0300

    adição da rota userposts

--- BACKEND/src/routes/index.js
+++ BACKEND/src/routes/index.js
@@ -3,10 +3,15 @@ import userRoutes from "./usersRoutes.js";
 import postsRoutes from "./postsRoutes.js";
 import {AuthController} from "../domain/auth/authUseCase/AuthController.js";
 import loginValidation from "../domain/auth/authValidation/index.js";
+import UserPostsValidation from "../domain/usersPosts/userPostsValidation/index.js";
+import { FindAllUserPostsController } from "../domain/usersPosts/useCase/findAllUserPostsController.js";
 
 const routes = express.Router();
 
+routes.get('/userposts/:id', UserPostsValidation.getAll, FindAllUserPostsController.listarAllUsersPost);
+
 routes.post("/login", loginValidation.login, AuthController.login);
+
 
 routes.use(userRoutes);
 routes.use(postsRoutes);
