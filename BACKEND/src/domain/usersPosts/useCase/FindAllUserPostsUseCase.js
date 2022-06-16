d63e4938930d35d540544b21ea85680747f5851b
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-16 16:55:15 -0300

    adição de infos dos usuários com os posts

--- BACKEND/src/domain/usersPosts/useCase/FindAllUserPostsUseCase.js
+++ BACKEND/src/domain/usersPosts/useCase/FindAllUserPostsUseCase.js
@@ -1,8 +1,16 @@
-import { Posts } from "../../models/index.js";
+import { Posts, Users } from "../../models/index.js";
 
 export class FindAllUserPostsUseCase {
    static async findAllUsersPosts(userID) {
-      const userPosts = await Posts.findAll({ where: { userID } });
+      const userPosts = await Posts.findAll({
+         where: { userID},
+         include: [
+            {
+               model: Users,
+               attributes: { exclude: ["password", "flag"] },
+            },
+         ],
+      });
       return userPosts;
    }
 }
