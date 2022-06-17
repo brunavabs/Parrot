3e7d11e26bf1668f27216ce19d523d5dd853fe21
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-17 02:04:17 -0300

    importante novo path para rotas

--- BACKEND/src/routes/index.js
+++ BACKEND/src/routes/index.js
@@ -4,11 +4,11 @@ import postsRoutes from "./postsRoutes.js";
 import {authController} from "../domain/auth/controller/index.js"
 import loginValidation from "../domain/auth/authValidation/index.js";
 import UserPostsValidation from "../domain/usersPosts/userPostsValidation/index.js";
-import { FindAllUserPostsController } from "../domain/usersPosts/useCase/findAllUserPostsController.js";
+import { findAllUserPostsController } from "../domain/usersPosts/controller/index.js";
 
 const routes = express.Router();
 
-routes.get('/userposts/:id', UserPostsValidation.getAll, FindAllUserPostsController.listarAllUsersPost);
+routes.get('/userposts/:id', UserPostsValidation.getAll, findAllUserPostsController.listarAllUsersPost);
 
 routes.post("/login", loginValidation.login, authController.login);
 
