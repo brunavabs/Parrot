6291420e85f303fa13d494ecf36e5587347ced1e
Lucas Veiga <lucasveigagonzaga@yahoo.com.br>
2022-06-17 01:38:03 -0300

    adicionando alertas com toast

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,15 +1,16 @@
 import { Provider } from 'react-redux';
-
 import { PersistGate }from 'redux-persist/integration/react'
-
 import { store , persistor } from './store';
-
 import AppRoutes from './routes'
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <ToastContainer autoClose={3000}/>
         <AppRoutes /> 
       </PersistGate>
     </Provider>
